9872c3596d66bb76b9dcd220c1482766039856fc
Rosmupo <95884239+Rosmupo@users.noreply.github.com>
2022-06-06 14:54:53 +0200

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String [] args){
+
+        Ordenador ordenador = new Ordenador();
+        System.out.println(ordenador.getMonthsList());
+        //FALTA IMPRIMIR LA INFO EN VEZ DEL PUNTERO
+
+    }
+}
---
+++ src/main/java/Ordenador.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Ordenador {
+
+    ArrayList<Month> monthlist;
+
+    public Ordenador() {
+        monthlist = new ArrayList<>();
+    }
+
+    public ArrayList<Month> getMonthsList(){
+
+        Month month0 = new Month("Enero");
+        Month month1 = new Month("Febrero");
+        Month month2 = new Month("Marzo");
+        Month month3 = new Month("Abril");
+        Month month4 = new Month("Mayo");
+        Month month5 = new Month("Junio");
+        Month month6 = new Month("Julio");
+        Month month7 = new Month("Agosto");
+        Month month8 = new Month("Septiembre");
+        Month month9 = new Month("Octubre");
+        Month month10 = new Month("Noviembre");
+        Month month11 = new Month("Diciembre");
+
+        monthlist.add(month0);
+        monthlist.add(month1);
+        monthlist.add(month2);
+        monthlist.add(month3);
+        monthlist.add(month4);
+        monthlist.add(month5);
+        monthlist.add(month6);
+        monthlist.add(month7);
+        monthlist.add(month8);
+        monthlist.add(month9);
+        monthlist.add(month10);
+        monthlist.add(month11);
+
+        return monthlist;
+    }
+
+}
+
